69389d9f9a8ad7dce13c424992b5ef088d3bb44a
LiuMing <460667926@qq.com>
2019-11-28 13:28:42 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
+
 
 class UsersController extends Controller
 {
@@ -18,9 +20,18 @@ class UsersController extends Controller
     	return view('users.edit',compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
-    {
-    	$user->update($request->all());
+    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
+    {	
+    	$data = $request->all();
+
+    	if($request->avatar) {
+    		if($result = $uploader->save($request->avatar,'avatar',$user->id)) {
+    			$data['avatar'] = $result['path'];
+    		}
+
+    	}
+
+    	$user->update($data);
     	return redirect()->route('users.show', $user->id)->with('success','个人资料更新成功！');
     }
 }
