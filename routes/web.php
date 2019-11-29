cefc02834e1dc3079246e8b56165e83abf05ab65
LiuMing <460667926@qq.com>
2019-11-29 10:38:18 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //用户信息
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+//话题
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
