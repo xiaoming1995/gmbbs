bd0a85645003da83cc232d45157d9c0f3a21964c
LiuMing <460667926@qq.com>
2019-11-29 18:32:45 +0000

    填充用户和话题数据

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-  	protected $timestamps = false;
+  	public $timestamps = false;
 
   	protected $fillable = ['name','description'];
 }
