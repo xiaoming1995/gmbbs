bad4b4fcfea4849716b5459de98e6e421875cd69
vagrant <vagrant@homestead>
2019-12-02 17:57:03 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -16,4 +16,38 @@ class Topic extends Model
     	return $this->belongsTo(User::class);
     }
 
+
+    /*
+    	本地作用域允许定义通用的约束集合以便在应用程序中重复使用。例如，你可能经常需要获取所有「流行」的用户。要定义这样一个范围，只需要在对应的 Eloquent 模型方法前添加 scope 前缀。
+    	文档：https://learnku.com/docs/laravel/6.x/eloquent/5176#local-scopes
+     */
+
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        // 按照创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
+
+
 }
