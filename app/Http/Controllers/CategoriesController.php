e5e8ac26af598e9ed5f76ff3b919eaf68ba9c464
vagrant <vagrant@homestead>
2019-12-02 17:58:55 +0000

    话题排序修改bug

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,7 +9,7 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $reqeust, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic)
     {
     	 // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
