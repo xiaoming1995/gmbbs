b76034f1000e69f2868d8473d2a72b16ccc36839
vagrant <vagrant@homestead>
2019-12-04 09:53:17 +0000

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //类别
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//图片上传
+Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
