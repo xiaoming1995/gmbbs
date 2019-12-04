2bee5cd21b0156bed8e2329d5689b311a2be1eca
vagrant <vagrant@homestead>
2019-12-04 07:41:52 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,16 @@ function category_nav_active($category_id)
 {
 	return active_class(if_route('categories.show') && if_route_param('category',$category_id));
 }
+
+
+//通过截取内容生成SEO  简介
+/*
+	strip_tags() 函数剥去字符串中的 HTML、XML 以及 PHP 的标签。
+	mb_strwidth — 返回字符串的宽度
+	Str::limit 限制内容宽度
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return Str::limit($excerpt, $length);
+}
