1ca3683959038ed64f441c57afde97b6b007c7b6
vagrant <vagrant@homestead>
2019-12-15 05:31:35 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -24,6 +25,9 @@ class ReplyObserver
     	//评论成功后留言统计回复数量
     	$reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
+
+         // 通知话题作者有新的评论
+        $reply->topic->user->toTopicNotify(new TopicReplied($reply));
     }
 
 }
