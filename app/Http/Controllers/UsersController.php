8155078264c7e5d0d7df8422b44c782b61ec61c9
vagrant <vagrant@homestead>
2019-12-20 16:47:37 +0000

    用户管理后台

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
     	$data = $request->all();
 
     	if($request->avatar) {
-    		if($result = $uploader->save($request->avatar,'avatar',$user->id,416)) {
+    		if($result = $uploader->save($request->avatar,'avatars',$user->id,416)) {
     			$data['avatar'] = $result['path'];
     		}
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -80,4 +80,40 @@ class User extends Authenticatable implements MustVerifyEmailContract
     }
 
 
+    /*
+        密码修改器
+
+        对密码进行自动加密 
+     */
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    /*
+        头像修改器
+
+        对后台上传的头像作以个路由的调整  因为后台的头像是异步的
+     */
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! \Str::startsWith($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
+
 }
