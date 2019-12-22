c6a857ba27bd1ee2b7b5ccd8fc9fa39156f1dba7
vagrant <vagrant@homestead>
2019-12-22 17:35:08 +0000

    侧边栏连接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,7 +10,7 @@ use App\Models\Category;
 use Auth;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
-
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -19,7 +19,7 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic,User $user)
+	public function index(Request $request, Topic $topic,User $user,Link $link)
 	{	
 		$topics =  $topic->withOrder($request->order)
 						 ->with('user','category')
@@ -27,7 +27,9 @@ class TopicsController extends Controller
 		// 活跃用户列表
 		$active_users = $user->getActiveUsers();
 
-		return view('topics.index', compact('topics','active_users'));
+		$links = $link->getAllCached();
+
+		return view('topics.index', compact('topics','active_users','links'));
 	}
 
     public function show(Request $request, Topic $topic)
--- config/administrator.php
+++ config/administrator.php
@@ -69,6 +69,7 @@ return array(
         ],
         '站点管理' => [
             'settings.site',
+            'links',
         ],
     ],
 
