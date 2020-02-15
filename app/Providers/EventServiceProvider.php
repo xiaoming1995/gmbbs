69991f1bd79c0d5fe3d15d0a94270e5707ecdce7
vagrant <vagrant@homestead>
2020-02-15 18:22:55 +0000

    add socialiteproviders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -23,6 +23,10 @@ class EventServiceProvider extends ServiceProvider
         \Illuminate\Auth\Events\Verified::class => [
             \App\Listeners\EmailVerified::class,
         ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            // add your listeners (aka providers) here
+            'SocialiteProviders\Weixin\WeixinExtendSocialite@handle'
+        ],
         // PasswordReset::class =>[
         //     \App\Listeners\PasswordReset::class,
         // ],  
